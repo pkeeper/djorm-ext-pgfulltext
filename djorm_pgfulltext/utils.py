1d66e1be0d1ce8c93cc9f2a7d8d7f275c13d05f8
Artem melanich <pensivekeeper@gmail.com>
2017-02-10 22:16:55 +0200

    U!

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -1,4 +1,6 @@
+from __future__ import unicode_literals
 import psycopg2
+import psycopg2.extensions
 
 from django.db import connection
 from django.utils.text import force_text
@@ -7,6 +9,12 @@ from django.utils.text import force_text
 def adapt(text):
     # make sure connection is open
     connection.cursor()
+    connection.connection.set_client_encoding('UNICODE')
+    text=text
+    print type(force_text(text))
+    # use Unicode
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODE, connection.connection)
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY, connection.connection)
     a = psycopg2.extensions.adapt(force_text(text))
     a.prepare(connection.connection)
-    return a
+    return unicode(a.getquoted(),'utf-8')
